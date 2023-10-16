a474611c4f9861d104bccad6f0fa45c0fd4e9e74
Arpan Gupta <arpan@pop-os.localdomain>
2023-10-16 17:38:20 +0530

    guessing game

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guess the number!");
+    let secret_num:u32 = rand::thread_rng().gen_range(1..=100);
+    loop {
+        let mut guess = String::new();
+
+        println!("Enter your guess.");
+        
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+    
+        let guess:u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+    
+        println!("You guessed: {guess}");
+    
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too Big"),
+            Ordering::Equal => {
+                println!("You win! Congratulations.");
+                break;
+            },
+        }
+    }
+}
